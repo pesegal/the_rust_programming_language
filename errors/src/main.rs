19c5b3238bdd4a0a0db583091d0799d94efd98c0
Peter Segal <pesegal@gmail.com>
2022-10-21 15:39:50 -0700

    matching example

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,4 +1,25 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
-    let v = vec![1,2,3];
-    v[99];
+
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+
+
+    // Panics will return traces
+    // let v = vec![1,2,3];
+    // v[99];
 }
