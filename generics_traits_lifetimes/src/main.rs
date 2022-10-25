6bce0b8cc68ab825967b80bb1cc45b3f5c62e053
Peter Segal <pesegal@gmail.com>
2022-10-24 17:10:54 -0700

    extracting to function example

---
+++ generics_traits_lifetimes/src/main.rs
@@ -0,0 +1,19 @@
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn main() {
+    let number_list = vec![24, 50, 25, 100, 65];
+
+    println!("The largest number is {}", largest(&number_list));
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    println!("The largest number is {}", largest(&number_list));
+}
