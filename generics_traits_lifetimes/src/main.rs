e7bb29dbd9702d3a2338156f08de6d21fcef7780
Peter Segal <pesegal@gmail.com>
2022-10-28 14:46:57 -0700

    example generic

--- generics_traits_lifetimes/src/main.rs
+++ generics_traits_lifetimes/src/main.rs
@@ -18,14 +18,26 @@ fn largest_char(list: &[char]) -> &char {
     largest
 }
 
+// Since the above methods have the exact same implementation we can use generics
+
+fn largest<T>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 fn main() {
     let number_list = vec![24, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }
